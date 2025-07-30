51d6438ee92326891e84b7c44e8511b1baf6cc2f
Joachim Bono <joachim.bono@protonmail.com>
2025-07-31 00:18:03 +0200

    refactor: split into separate file for each part

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -1,54 +1,10 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"regexp"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	// do() OR don't() OR mul(x, y)
-	pattern := `do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)`
-
-	re, err := regexp.Compile(pattern)
-	if err != nil {
-		log.Fatal("Invalid regex pattern:", err)
-	}
-
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatal("Error opening file:", err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	acc := 0
-	enabled := true
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		matches := re.FindAllStringSubmatch(line, -1)
-		for _, match := range matches {
-			if match[0] == "do()" {
-				enabled = true
-			} else if match[0] == "don't()" {
-				enabled = false
-			} else if match[0][:3] == "mul" {
-				if enabled {
-					a, _ := strconv.Atoi(match[1])
-					b, _ := strconv.Atoi(match[2])
-					product := a * b
-					acc += product
-				}
-			}
-		}
-	}
-	fmt.Printf("Sum of products: %d\n", acc)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error reading file:", err)
-	}
+	fmt.Printf("Running part1...\n")
+	Part1()
+	fmt.Printf("Running part2...\n")
+	Part2()
 }
---
+++ 2024/day03/part2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func Part2() {
+	// do() OR don't() OR mul(x, y)
+	pattern := `do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\)`
+
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Fatal("Invalid regex pattern:", err)
+	}
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal("Error opening file:", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	acc := 0
+	enabled := true
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := re.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "do()" {
+				enabled = true
+			} else if match[0] == "don't()" {
+				enabled = false
+			} else if match[0][:3] == "mul" {
+				if enabled {
+					a, _ := strconv.Atoi(match[1])
+					b, _ := strconv.Atoi(match[2])
+					product := a * b
+					acc += product
+				}
+			}
+		}
+	}
+	fmt.Printf("Sum of products: %d\n", acc)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+}
