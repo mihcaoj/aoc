d987c33d69011cb4086b0ce714b44b05f54d9d9b
Joachim Bono <joachim.bono@protonmail.com>
2025-08-13 19:01:41 +0200

    refactor: day11 part1

--- 2024/day11/part1.go
+++ 2024/day11/part1.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -10,40 +9,30 @@ import (
 )
 
 func partOne() {
-	file, err := os.Open("input.txt")
+	file, err := os.ReadFile("input.txt")
 	if err != nil {
-		log.Fatal("Error opening file:", err)
+		log.Fatal("Error reading file:", err)
 	}
-	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
 	var stones []int
-	for scanner.Scan() {
-		line := scanner.Text()
-		for part := range strings.FieldsSeq(line) {
-			num, _ := strconv.Atoi(part)
-			stones = append(stones, num)
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal("Error scanning file:", err)
+	for part := range strings.FieldsSeq(string(file)) {
+		num, _ := strconv.Atoi(part)
+		stones = append(stones, num)
 	}
 
 	for range 25 {
 		var newStones []int
 		for _, stone := range stones {
+			s := strconv.Itoa(stone)
 			if stone == 0 {
 				newStones = append(newStones, 1)
+			} else if len(s)%2 == 0 {
+				mid := len(s) / 2
+				left, _ := strconv.Atoi(s[:mid])
+				right, _ := strconv.Atoi(s[mid:])
+				newStones = append(newStones, left, right)
 			} else {
-				s := strconv.Itoa(stone)
-				if len(s)%2 == 0 {
-					mid := len(s) / 2
-					left, _ := strconv.Atoi(s[:mid])
-					right, _ := strconv.Atoi(s[mid:])
-					newStones = append(newStones, left, right)
-				} else {
-					newStones = append(newStones, stone*2024)
-				}
+				newStones = append(newStones, stone*2024)
 			}
 		}
 		stones = newStones
