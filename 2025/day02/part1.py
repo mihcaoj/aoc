8a34bc6f1d09d09480b47b346a2316d0275867cb
Joachim Bono <joachim.bono@protonmail.com>
2025-12-14 17:34:55 +0100

    add: day02 part1 2025

---
+++ 2025/day02/part1.py
@@ -0,0 +1,21 @@
+import re
+
+invalid_id = re.compile(r'^([1-9]\d*)\1$')  # number repeated twice with no leading zero
+total = 0
+
+with open("input.txt", "r") as f:
+    for line in f:
+        parts = line.strip().split(",")
+        for part in parts:
+            part = part.strip()
+
+            start_str, end_str = part.split("-", 1)
+            start_num = int(start_str.strip())
+            end_num = int(end_str.strip())
+
+            # Loop over all numbers in the range
+            for n in range(start_num, end_num + 1):
+                if invalid_id.fullmatch(str(n)):
+                    total += n
+
+print("Sum:", total)
